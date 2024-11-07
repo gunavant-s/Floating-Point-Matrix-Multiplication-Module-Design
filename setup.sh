8baff6dd27238d6a675f7d995cb850a6968c367f
Gunavant <139606020+gunavant-s@users.noreply.github.com>
2024-11-06 20:50:30 -0500

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+complete -W "\`grep -oE '^[a-zA-Z0-9_.-]+:([^=]|$)' Makefile | sed 's/[^a-zA-Z0-9_.-]*$//'\`" make
+module load modelsim/2021.2
+module load synopsys/2022
+module load xcelium/24.03
