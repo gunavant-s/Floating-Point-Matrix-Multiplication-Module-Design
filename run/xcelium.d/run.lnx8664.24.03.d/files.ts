f456633fa8d67b5725aa4e60bce8226ff5456b79
Gunavant <139606020+gunavant-s@users.noreply.github.com>
2024-11-06 20:58:17 -0500

    Add files via upload

---
+++ run/xcelium.d/run.lnx8664.24.03.d/files.ts
@@ -0,0 +1,3 @@
+1730459400 /mnt/ncsudrive/g/gsetty/fall_2024/564/hw6/mini_project/testbench/testbench.sv
+1727716029 /mnt/ncsudrive/g/gsetty/fall_2024/564/hw6/mini_project/testbench/sram.sv
+1730942333 /mnt/ncsudrive/g/gsetty/fall_2024/564/hw6/mini_project/rtl/dut.sv
